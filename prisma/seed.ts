dd3e7b46477b7240757fde586e4a717d8d9bb238
bsilawko <mrufkazkrem@protonmail.com>
2024-11-18 14:09:59 +0100

    create 5 new addresses for restaurants

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,35 +9,54 @@ async function initData() {
       email: "balbinka@gmail.com",
       gender: "F",
       meeting_status: false,
-      points: 0
+      points: 0,
     },
     where: {
       email: "balbinka@gmail.com",
     },
   });
+  const addresses = [
+    {
+      name: "Marszałkowska 55/73, 00-676 Warszawa",
+      X_coords: 52.22384809269264,
+      Y_coords: 21.014972257671275,
+    },
+    {
+      name: "999, Lwowska 1, 00-660 Warszawa",
+      X_coords: 52.222647666815845,
+      Y_coords: 21.011307754440278,
+    },
+    {
+      name: "Lwowska 1, 00-660 Warszawa",
+      X_coords: 52.22252816861938,
+      Y_coords: 21.0111360496488,
+    },
+    {
+      name: "Marszałkowska 28, 00-576 Warszawa",
+      X_coords: 52.21953492198973,
+      Y_coords: 21.018708227263527,
+    },
+    {
+      name: "Jana i Jędrzeja Śniadeckich 23, 00-654 Warszawa",
+      X_coords: 52.22049482276576,
+      Y_coords: 21.0120639520506,
+    },
+  ];
+  for (const address of addresses) {
+    await prisma.address.upsert({
+      where: { name: address.name },
+      update: address,
+      create: address,
+    });
+  }
   await prisma.restaurant.upsert({
     where: {
       name: "Marszałkowski bar mleczny",
     },
-    update: {
-      description: "smakuwa",
-      opening_hour_mon: 8,
-      opening_hour_tue: 8,
-      opening_hour_wen: 8,
-      opening_hour_thu: 8,
-      opening_hour_fri: 8,
-      opening_hour_sat: 8,
-      opening_hour_sun: 8,
-      closing_hour_mon: 19,
-      closing_hour_tue: 19,
-      closing_hour_wen: 19,
-      closing_hour_thu: 19,
-      closing_hour_fri: 19,
-      closing_hour_sat: 19,
-      closing_hour_sun: 19,
-    },
+    update: { address_id: 1 },
     create: {
       name: "Marszałkowski bar mleczny",
+      address_id: 1,
       description: "smakuwa",
       opening_hour_mon: 8,
       opening_hour_tue: 8,
@@ -60,25 +79,10 @@ async function initData() {
     where: {
       name: "Kebab dubai",
     },
-    update: {
-      description: "prawdziwy kebab piecze dwa razy",
-      opening_hour_mon: 8,
-      opening_hour_tue: 8,
-      opening_hour_wen: 8,
-      opening_hour_thu: 8,
-      opening_hour_fri: 8,
-      opening_hour_sat: 8,
-      opening_hour_sun: 8,
-      closing_hour_mon: 19,
-      closing_hour_tue: 19,
-      closing_hour_wen: 19,
-      closing_hour_thu: 19,
-      closing_hour_fri: 19,
-      closing_hour_sat: 19,
-      closing_hour_sun: 19,
-    },
+    update: { address_id: 2 },
     create: {
       name: "Kebab dubai",
+      address_id: 2,
       description: "prawdziwy kebab piecze dwa razy",
       opening_hour_mon: 8,
       opening_hour_tue: 8,
@@ -97,9 +101,10 @@ async function initData() {
     },
   });
   await prisma.restaurant.upsert({
-    update: {},
+    update: { address_id: 3 },
     create: {
       name: "Kebab king",
+      address_id: 3,
       description: "prawdziwy kebab piecze dwa razy",
       opening_hour_mon: 8,
       opening_hour_tue: 8,
@@ -118,12 +123,13 @@ async function initData() {
     },
     where: {
       name: "Kebab king",
-    }
+    },
   });
   await prisma.restaurant.upsert({
-    update: {},
+    update: { address_id: 4 },
     create: {
-      name: "Kebab relax",
+      name: "Moza Kebab",
+      address_id: 4,
       description: "prawdziwy kebab piecze dwa razy",
       opening_hour_mon: 8,
       opening_hour_tue: 8,
@@ -141,13 +147,14 @@ async function initData() {
       closing_hour_sun: 19,
     },
     where: {
-      name: "Kebab relax",
-    }
+      name: "Moza Kebab",
+    },
   });
-    await prisma.restaurant.upsert({
-    update: {},
+  await prisma.restaurant.upsert({
+    update: { address_id: 5 },
     create: {
-      name: "Kebab habiba",
+      name: "Amman Kebab",
+      address_id: 5,
       description: "prawdziwy kebab piecze dwa razy",
       opening_hour_mon: 8,
       opening_hour_tue: 8,
@@ -165,8 +172,8 @@ async function initData() {
       closing_hour_sun: 19,
     },
     where: {
-      name: "Kebab habiba",
-    }
+      name: "Amman Kebab",
+    },
   });
 }
 
