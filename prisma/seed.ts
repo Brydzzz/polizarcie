664122bafababb7aa03979f3d194f29d427503f5
Elbergg <61651967+Elbergg@users.noreply.github.com>
2024-11-29 18:28:07 +0100

    adding init data to seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -54,6 +54,93 @@ async function initData() {
       create: address,
     });
   }
+
+  const dishes = [
+    {
+      dish_id: 1,
+      name: "Kebab średni",
+      description: "Baranina/wołowina, sórówka, pita",
+      price: 23,
+      restaurant_id: 5,
+    },
+    {
+      dish_id: 2,
+      name: "Kebab mały",
+      description: "Baranina/wołowina, sórówka, pita",
+      price: 20,
+      restaurant_id: 5,
+    },
+    {
+      dish_id: 3,
+      name: "Kebab duży",
+      description: "Baranina/wołowina, sórówka, pita",
+      price: 26,
+      restaurant_id: 5,
+    },
+    {
+      dish_id: 3,
+      name: "Talerz kebab",
+      description: "Baranina/wołowina, sórówkai i frytki razem na talerzu",
+      price: 30,
+      restaurant_id: 5,
+    },
+    {
+      dish_id: 3,
+      name: "Fryto kebab",
+      description: "Baranina/wołowina, sórówka i, frytki, w picie",
+      price: 25,
+      restaurant_id: 5,
+    },
+  ];
+  for (const dish of dishes) {
+    await prisma.dish.upsert({
+      where: { dish_id: dish.dish_id },
+      update: dish,
+      create: dish,
+    });
+  }
+
+
+
+  const reviews = [
+    {
+      review_id: 1,
+      content: "Wybitny kebab",
+      restaurant_id:   5,
+      points:    5,
+      spent_per_person: 23,
+      date: '2024-11-25',
+      who_wrote : 1
+    },
+    {
+      review_id: 2,
+      content: "Nawet nienajgorszy",
+      restaurant_id:   5,
+      points:    4,
+      spent_per_person: 26,
+      date: '2024-10-19',
+      who_wrote : 2
+    },
+    {
+      review_id: 3,
+      content: "Obrzydliwy! Już nigdy tam nie zjem!",
+      restaurant_id:   5,
+      points:    1,
+      spent_per_person: 20,
+      date: '2024-10-31',
+      who_wrote : 3
+    },
+  ];
+  for (const review of reviews) {
+    await prisma.review.upsert({
+      where: { review_id: review.review_id },
+      update: review,
+      create: review,
+    });
+  }
+
+
+
   await prisma.restaurant.upsert({
     where: {
       name: "Marszałkowski bar mleczny",
@@ -86,6 +173,7 @@ async function initData() {
     },
     update: { address_id: 2 },
     create: {
+      restaurant_id: 5,
       name: "Kebab dubai",
       address_id: 2,
       description: "prawdziwy kebab piecze dwa razy",
@@ -180,7 +268,6 @@ async function initData() {
       name: "Amman Kebab",
     },
   });
-}
 
 initData()
   .then(async () => {
@@ -191,3 +278,4 @@ initData()
     await prisma.$disconnect();
     process.exit(1);
   });
+}
