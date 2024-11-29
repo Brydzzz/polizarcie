7e12699af011a9fbe7a0a79c136ef7ff72c67226
bsilawko <mrufkazkrem@protonmail.com>
2024-11-29 16:11:19 +0100

    basic layout restaurantpage

--- app/(content)/restaurant/[id]/page.tsx
+++ app/(content)/restaurant/[id]/page.tsx
@@ -1,12 +1,15 @@
 "use client";
+import StarInput from "@/components/inputs/star-input.component";
 import { get_restaurant_by_id } from "@/utils/prisma/restaurants";
-import { Restaurant } from "@prisma/client";
+import { Address, Restaurant } from "@prisma/client";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import styles from "./page.module.scss";
 
 const RestaurantPage = () => {
   const { id } = useParams<{ id: string }>();
-  const [restaurant, setRestaurant] = useState<Restaurant | null>(null);
+  const [restaurant, setRestaurant] =
+    useState<Restaurant & { address: Partial<Address> | null }>();
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
@@ -22,8 +25,29 @@ const RestaurantPage = () => {
   }, [id]);
   if (error) return <div>Error: {error}</div>;
   return (
-  <h1>{restaurant?.name}</h1>
-);
+    <main className={styles.restaurantPage}>
+      <div className={styles.header}>
+        <p>{restaurant?.name}</p>
+      </div>
+
+      <div className={styles.content}>
+        <div className={styles.mapContainer}>Map</div>
+        <div className={styles.rating}>
+          <StarInput value={4} max={5} disabled></StarInput>
+          <p>4,2</p>
+        </div>
+        <div className={styles.info}>
+          <p>
+            <strong>Adres:</strong> {restaurant?.address?.name}
+          </p>
+          <p>
+            <strong>Opis:</strong> {restaurant?.description}
+          </p>
+        </div>
+        <div className={styles.hours}></div>
+      </div>
+    </main>
+  );
 };
 
 export default RestaurantPage;
