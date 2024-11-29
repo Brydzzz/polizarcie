dc4de3c115ebcc7a8eee2f4d23eb99fb2e9678db
bsilawko <mrufkazkrem@protonmail.com>
2024-11-29 18:22:02 +0100

    added loading indicator and more elements

--- app/(content)/restaurant/[id]/page.tsx
+++ app/(content)/restaurant/[id]/page.tsx
@@ -1,4 +1,10 @@
 "use client";
+import Button from "@/components/button/button.component";
+import {
+  ButtonColor,
+  ButtonSize,
+  ButtonStyle,
+} from "@/components/button/button.types";
 import StarInput from "@/components/inputs/star-input.component";
 import { get_restaurant_by_id } from "@/utils/prisma/restaurants";
 import { Address, Restaurant } from "@prisma/client";
@@ -27,30 +33,104 @@ const RestaurantPage = () => {
     };
     fetchRestaurant();
   }, [id]);
-  if (loading) return <div>Loading...</div>;
+  if (loading)
+    return (
+      <div className={styles.loading}>
+        <h1>Loading...</h1>
+      </div>
+    );
 
   if (error) return <div>Error: {error}</div>;
+
+  const hours = [
+    {
+      day: "Pon",
+      opening: restaurant?.opening_hour_mon,
+      closing: restaurant?.closing_hour_mon,
+    },
+    {
+      day: "Wt",
+      opening: restaurant?.opening_hour_tue,
+      closing: restaurant?.closing_hour_tue,
+    },
+    {
+      day: "Śr",
+      opening: restaurant?.opening_hour_wen,
+      closing: restaurant?.closing_hour_wen,
+    },
+    {
+      day: "Czw",
+      opening: restaurant?.opening_hour_thu,
+      closing: restaurant?.closing_hour_thu,
+    },
+    {
+      day: "Pt",
+      opening: restaurant?.opening_hour_fri,
+      closing: restaurant?.closing_hour_fri,
+    },
+    {
+      day: "Sob",
+      opening: restaurant?.opening_hour_sat,
+      closing: restaurant?.closing_hour_sat,
+    },
+    {
+      day: "Nd",
+      opening: restaurant?.opening_hour_sun,
+      closing: restaurant?.closing_hour_sun,
+    },
+  ];
+
   return (
     <main className={styles.restaurantPage}>
       <div className={styles.header}>
         <p>{restaurant?.name}</p>
+        {/* TODO: heart input for adding to favorite restaurants */}
       </div>
 
       <div className={styles.content}>
-        <div className={styles.mapContainer}>Map</div>
-        <div className={styles.rating}>
-          <StarInput value={4} max={5} disabled></StarInput>
-          <p>4,2</p>
+        <div className={styles.column}>
+          <div className={styles.mapContainer}>Map</div>
         </div>
-        <div className={styles.info}>
-          <p>
-            <strong>Adres:</strong> {restaurant?.address?.name}
-          </p>
-          <p>
-            <strong>Opis:</strong> {restaurant?.description}
-          </p>
+        <div className={styles.column}>
+          <div className={styles.rating}>
+            {/* TODO: get actual rating for restaurant */}
+            <StarInput value={4} max={5} disabled></StarInput>
+            <p>4,2</p>
+          </div>
+          <div className={styles.info}>
+            <h3>Adres:</h3>
+            <p>{restaurant?.address?.name}</p>
+            <h3>Opis:</h3>
+            <p>{restaurant?.description}</p>
+          </div>
         </div>
-        <div className={styles.hours}></div>
+        <div className={styles.column}>
+          <div className={styles.hours}>
+            <h3>Godziny Otwarcia:</h3>
+            <ul>
+              {hours.map((hour, index) => (
+                <li key={index}>
+                  <div className={styles.open_hours}>
+                    <p>
+                      <strong>{hour.day}:</strong>
+                    </p>
+                    <p>
+                      {hour.opening}:00 - {hour.closing}:00
+                    </p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          {/* TODO: button OnClick */}
+          <Button
+            style={ButtonStyle.SOLID}
+            color={ButtonColor.PRIMARY}
+            size={ButtonSize.NORMAL}
+          >
+            Napisz Opinię
+          </Button>
+        </div>
       </div>
     </main>
   );
