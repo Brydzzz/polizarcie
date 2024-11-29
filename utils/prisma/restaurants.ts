389fd8e8ec5cd40d2bbeced171fe7775ca8de37c
bsilawko <mrufkazkrem@protonmail.com>
2024-11-29 14:45:25 +0100

    implemented get_restaurant_by_id()

--- utils/prisma/restaurants.ts
+++ utils/prisma/restaurants.ts
@@ -1,19 +1,29 @@
-"use server"
+"use server";
 
-import { prisma } from "@/utils/prisma"
-import { Restaurant } from "@prisma/client"
-import { json } from "stream/consumers";
+import { prisma } from "@/utils/prisma";
 
 export async function get_all_restaurants() {
-    const restaurants = await prisma.restaurant.findMany();
-    return restaurants
+  const restaurants = await prisma.restaurant.findMany();
+  return restaurants;
 }
 
-export async function get_restaurants_like(like:string) {
-    const restaurants = await prisma.restaurant.findMany({
-        where: {
-            name: { contains: like, mode: 'insensitive' }
-        }
-    });
-    return restaurants;
+export async function get_restaurants_like(like: string) {
+  const restaurants = await prisma.restaurant.findMany({
+    where: {
+      name: { contains: like, mode: "insensitive" },
+    },
+  });
+  return restaurants;
+}
+
+export async function get_restaurant_by_id(id: number) {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      restaurant_id: id,
+    },
+  });
+  if (!restaurant) {
+    throw new Error(`Restaurant with ID ${id} not found`);
+  }
+  return restaurant;
 }
