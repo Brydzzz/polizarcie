061d8917c20a31312feb19a775dbc41406458506
bsilawko <mrufkazkrem@protonmail.com>
2024-11-29 20:23:11 +0100

    Implemented map view component

---
+++ app/dev/map/page.tsx
@@ -0,0 +1,16 @@
+import MapView from "@/components/map-view.component";
+
+const MapPage = () => {
+  return (
+    <div
+      className="centralized-x"
+      style={{ marginTop: "100px", paddingBottom: "100px" }}
+    >
+      <div className="centralized-y" style={{ width: "500px" }}>
+        <MapView X_coord={21.0148725} Y_coord={52.2237223}></MapView>
+      </div>
+    </div>
+  );
+};
+
+export default MapPage;
