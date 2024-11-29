5e4ded5af4e966e36a9b0fc3e1414db0cf455efc
Elbergg <61651967+Elbergg@users.noreply.github.com>
2024-11-29 21:51:40 +0100

    searching by address

--- utils/prisma/restaurants.ts
+++ utils/prisma/restaurants.ts
@@ -1,28 +1,37 @@
-"use server"
+"use server";
 
 import { prisma } from "@/utils/prisma";
 
 export async function get_all_restaurants() {
-    const restaurants = await prisma.restaurant.findMany({
-        include: { address: {
-            select: {
-            name: true,
-        }}},
-    });
-    return restaurants
+  const restaurants = await prisma.restaurant.findMany({
+    include: {
+      address: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+  return restaurants;
 }
 
-export async function get_restaurants_like(like:string) {
-    const restaurants = await prisma.restaurant.findMany({
-        include: { address: {
-            select: {
-            name: true,
-        }}},
-        where: {OR: [{
-            name: { contains: like, mode: 'insensitive' }},
-            {address: {name: { contains: like, mode: 'insensitive'}}}
-        ]},
-
-    });
-    return restaurants;
+export async function get_restaurants_like(like: string) {
+  const restaurants = await prisma.restaurant.findMany({
+    include: {
+      address: {
+        select: {
+          name: true,
+        },
+      },
+    },
+    where: {
+      OR: [
+        {
+          name: { contains: like, mode: "insensitive" },
+        },
+        { address: { name: { contains: like, mode: "insensitive" } } },
+      ],
+    },
+  });
+  return restaurants;
 }
