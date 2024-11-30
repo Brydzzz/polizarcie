722d9a0ab4fb9aef9ea4008808225b6beaa6b24c
Rys00 <mateusz.ogniewski@gmail.com>
2024-11-30 15:36:48 +0100

    minor visual changes

--- app/page.tsx
+++ app/page.tsx
@@ -10,23 +10,27 @@ export default async function Home() {
   return (
     <main className={styles.container}>
       <LandingHeader />
-      <CenterSection width="600px" offsetX="-200px">
+      <CenterSection width="800px" offsetX="-200px">
         <h1>Co to jest?</h1>
-        Poliżarcie to portal dla prawdziwych politechnicznych żarłoków. Jesteś
-        głodny, a przed tobą jeszcze 4 godziny labów? Długie okienko i nie wiesz
-        co ze sobą zrobić? Czy może po prostu chcesz zjeść coś fajnego ze
-        znajomymi ze studiów? Nie martw się, Poliżarcie to miejsce dla ciebie!
-        Możesz wyszukać interesujący cię lokal, sprawdzić gdzie się znajduje,
-        zerknąc na menu i ceny przeczytać recenzje albo nawet dodać własne.
-        Wszystko jest możliwe dzięki Poliżarciu!
+        <p>
+          Poliżarcie to portal dla prawdziwych politechnicznych żarłoków. Jesteś
+          głodny, a przed tobą jeszcze 4 godziny labów? Długie okienko i nie
+          wiesz co ze sobą zrobić? Czy może po prostu chcesz zjeść coś fajnego
+          ze znajomymi ze studiów? Nie martw się, Poliżarcie to miejsce dla
+          ciebie! Możesz wyszukać interesujący cię lokal, sprawdzić gdzie się
+          znajduje, zerknąć na menu i ceny przeczytać recenzje albo nawet dodać
+          własne. Wszystko jest możliwe dzięki Poliżarciu!
+        </p>
       </CenterSection>
       <section className={styles.light}>
-        <CenterSection width="600px" offsetX="200px">
+        <CenterSection width="800px" offsetX="200px">
           <h1>Chcesz kogoś poznać?</h1>
-          Nie martw się! Poliżarcie wprowadza innowacyjny system łączenia
-          użytkowników na podstawie ich ulubionych restauracji. Dzięki temu
-          możesz miło spędzić przerwę z kimś nowym. A kto wie? Może wyniknie z
-          tego coś więcej...
+          <p>
+            Nie martw się! Poliżarcie wprowadza innowacyjny system łączenia
+            użytkowników na podstawie ich ulubionych restauracji. Dzięki temu
+            możesz miło spędzić przerwę z kimś nowym. A kto wie? Może wyniknie z
+            tego coś więcej...
+          </p>
         </CenterSection>
       </section>
     </main>
