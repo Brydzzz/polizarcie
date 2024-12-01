bff5398c067ebe546921661bf2f8c84059ec513a
bsilawko <mrufkazkrem@protonmail.com>
2024-12-01 21:43:05 +0100

    added smooth scroll down to menuReviewSection

--- components/sections/menu-review-section.component.tsx
+++ components/sections/menu-review-section.component.tsx
@@ -2,7 +2,7 @@
 import { useAppDispatch, useAppSelector } from "@/lib/store/hooks";
 import { selectRestaurantPageView } from "@/lib/store/layout-options/layout-options.selector";
 import { setRestaurantPageView } from "@/lib/store/layout-options/layout-options.slice";
-import { ReactNode, useEffect } from "react";
+import { ReactNode, useEffect, useRef } from "react";
 import Button from "../button/button.component";
 import { ButtonSize, ButtonStyle } from "../button/button.types";
 import styles from "./menu-review-section.module.scss";
@@ -21,25 +21,46 @@ const MenuReviewSection = ({ reviewList, menuList }: Props) => {
     };
   }, [dispatch]);
 
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  const scrollToSection = () => {
+    const headerHeight = 280;
+    const offset = (sectionRef.current?.offsetTop || 0) - headerHeight;
+    window.scrollTo({
+      top: offset,
+      behavior: "smooth",
+    });
+  };
+
+  const handleMenuClick = () => {
+    dispatch(setRestaurantPageView("menu"));
+    scrollToSection();
+  };
+
+  const handleReviewsClick = () => {
+    dispatch(setRestaurantPageView("reviews"));
+    scrollToSection();
+  };
+
   return (
     <div className={styles.menuReviewSection}>
       <div className={styles.changeSection}>
         <Button
           style={view === "menu" ? ButtonStyle.SOLID : ButtonStyle.BACKDROP}
           size={ButtonSize.LARGE}
-          onClick={() => dispatch(setRestaurantPageView("menu"))}
+          onClick={handleMenuClick}
         >
           Menu
         </Button>
         <Button
           style={view === "reviews" ? ButtonStyle.SOLID : ButtonStyle.BACKDROP}
           size={ButtonSize.LARGE}
-          onClick={() => dispatch(setRestaurantPageView("reviews"))}
+          onClick={handleReviewsClick}
         >
           Opinie
         </Button>
       </div>
-      <div className={styles.section}>
+      <div ref={sectionRef} className={styles.section}>
         {view === "menu" ? menuList : reviewList}
       </div>
     </div>
