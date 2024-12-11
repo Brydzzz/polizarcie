3d4d21477f74a18b4dbebdcb1c0095ce5df630b0
Elbergg <61651967+Elbergg@users.noreply.github.com>
2024-12-11 23:37:37 +0100

    implemented current progress

--- components/cards/match-card-component.tsx
+++ components/cards/match-card-component.tsx
@@ -3,6 +3,7 @@
 import { getTopLikedRests } from "@/lib/db/users";
 import { User } from "@prisma/client";
 import styles from "./match-card.module.scss";
+import UserCard from "./user-desc-card-component";
 type Props = {
   data: Partial<User>;
 };
@@ -13,7 +14,12 @@ const MatchCard = async ({ data }: Props) => {
   const likedRests = await getTopLikedRests(String(id));
   return (
     <div className={styles.container}>
-      <div className={styles.photo}></div>
+      <div className={styles.photoBox}>
+        <div className={styles.photo}></div>
+      </div>
+      <div className={styles.details}>
+        <UserCard data={data} socials={false}></UserCard>
+      </div>
     </div>
   );
 };
