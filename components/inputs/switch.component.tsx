9fa2e47ab07ec67e023a6e94293615db8d66e55b
bsilawko <mrufkazkrem@protonmail.com>
2024-12-12 21:42:21 +0100

    changed label style

--- components/inputs/switch.component.tsx
+++ components/inputs/switch.component.tsx
@@ -1,5 +1,6 @@
 import styles from "./switch.module.scss";
 
+// TODO add style props and disabled
 type Props = {
   checked: boolean;
   label?: string;
@@ -9,7 +10,7 @@ type Props = {
 const Switch = ({ checked, label, onChange }: Props) => {
   return (
     <div className={styles.switch_toggle}>
-      {label && <span>{label}</span>}
+      {label && <h3>{label}</h3>}
       <div
         className={`${styles.switch} ${checked ? styles.checked : ""}`}
         onClick={() => onChange(!checked)}
