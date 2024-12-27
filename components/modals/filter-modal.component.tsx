06c15b52f2ed86af3ffadbeb81f6e23e8f570189
bsilawko <mrufkazkrem@protonmail.com>
2024-12-27 18:03:57 +0100

    hardcoded faculties and clearFilters

--- components/modals/filter-modal.component.tsx
+++ components/modals/filter-modal.component.tsx
@@ -14,56 +14,133 @@ type Props = {
   onCancelButtonClick?: MouseEventHandler;
 };
 
+type FacultyState = {
+  value: string;
+  x?: number;
+  y?: number;
+};
+
 const FilterModal = ({ onCancelButtonClick }: Props) => {
   const sortOptions = [
     { name: "Nazwa", value: "name" },
     { name: "Ocena", value: "rating" },
     { name: "Cena na osobę", value: "price" },
   ];
+
   const facultyOptions = [
     { name: "Wybierz", value: "none" },
-    { name: "Kolegium Nauk Ekonomicznych i Społecznych", value: "knes" },
-    { name: "Wydział Administracji i Nauk Społecznych", value: "admin_social" },
-    { name: "Wydział Architektury", value: "arch" },
     {
-      name: "Wydział Budownictwa, Mechaniki i Petrochemii",
-      value: "civil_mech_petro",
+      name: "Wydział Administracji i Nauk Społecznych",
+      value: "admin_social",
+      x: 21.009993,
+      y: 52.220656,
     },
-    { name: "Wydział Chemiczny", value: "chemical" },
-    { name: "Wydział Elektroniki i Technik Informacyjnych", value: "weiti" },
-    { name: "Wydział Elektryczny", value: "electric" },
-    { name: "Wydział Fizyki", value: "physics" },
-    { name: "Wydział Geodezji i Kartografii", value: "geodesy_cartography" },
+    { name: "Wydział Architektury", value: "arch", x: 21.012836, y: 52.22219 },
+    { name: "Wydział Chemiczny", value: "chemical", x: 21.00881, y: 52.221592 },
+    {
+      name: "Wydział Elektroniki i Technik Informacyjnych",
+      value: "weiti",
+      x: 21.011755,
+      y: 52.219033,
+    },
+    {
+      name: "Wydział Elektryczny",
+      value: "electric",
+      x: 52.221538,
+      y: 21.00613,
+    },
+    { name: "Wydział Fizyki", value: "physics", x: 21.007222, y: 52.221425 },
+    {
+      name: "Wydział Geodezji i Kartografii",
+      value: "geodesy_cartography",
+      x: 21.009993,
+      y: 52.220656,
+    },
     {
       name: "Wydział Instalacji Budowlanych, Hydrotechniki i Inżynierii Środowiska",
       value: "building_hydrotechnics",
+      x: 21.007624,
+      y: 52.220423,
     },
     {
       name: "Wydział Inżynierii Chemicznej i Procesowej",
       value: "chemical_process",
+      x: 21.015521,
+      y: 52.213882,
     },
-    { name: "Wydział Inżynierii Lądowej", value: "land_engineering" },
-    { name: "Wydział Inżynierii Materiałowej", value: "material_engineering" },
-    { name: "Wydział Matematyki i Nauk Informacyjnych", value: "mini" },
+    {
+      name: "Wydział Inżynierii Lądowej",
+      value: "land_engineering",
+      x: 21.011696,
+      y: 52.217717,
+    },
+    {
+      name: "Wydział Inżynierii Materiałowej",
+      value: "material_engineering",
+      x: 20.999427,
+      y: 52.201308,
+    },
+    {
+      name: "Wydział Matematyki i Nauk Informacyjnych",
+      value: "mini",
+      x: 21.006959,
+      y: 52.222141,
+    },
     {
       name: "Wydział Mechaniczny Energetyki i Lotnictwa",
       value: "mech_energy_aero",
+      x: 21.005296,
+      y: 52.221208,
     },
-    { name: "Wydział Mechaniczny Technologiczny", value: "mech_tech" },
-    { name: "Wydział Mechatroniki", value: "mech" },
+    {
+      name: "Wydział Mechaniczny Technologiczny",
+      value: "mech_tech",
+      x: 21.002724,
+      y: 52.203302,
+    },
+    { name: "Wydział Mechatroniki", value: "mech", x: 21.000881, y: 52.20293 },
     {
       name: "Wydział Samochodów i Maszyn Roboczych",
       value: "vehicles_machines",
+      x: 21.002244,
+      y: 52.204328,
     },
-    { name: "Wydział Transportu", value: "transport" },
-    { name: "Wydział Zarządzania", value: "management" },
+    {
+      name: "Wydział Transportu",
+      value: "transport",
+      x: 21.008005,
+      y: 52.221988,
+    },
+    {
+      name: "Wydział Zarządzania",
+      value: "management",
+      x: 21.002721,
+      y: 52.203297,
+    },
   ];
 
   const [priceRange, setPriceRange] = useState({ min: 0, max: 100 });
   const [isOpen, setIsOpen] = useState(false);
   const [minRating, setMinRating] = useState(0);
   const [selectedSort, setSelectedSort] = useState("name");
-  const [selectedFaculty, setSelectedFaculty] = useState("none");
+  const [selectedFaculty, setSelectedFaculty] = useState<FacultyState>({
+    value: "none",
+  });
+
+  const handleFacultyChange = (value: string) => {
+    const faculty = facultyOptions.find((f) => f.value === value);
+    if (faculty) {
+      setSelectedFaculty({ value: faculty.value, x: faculty.x, y: faculty.y });
+    }
+  };
+
+  const clearFilters = () => {
+    setPriceRange({ min: 0, max: 100 });
+    setIsOpen(false);
+    setMinRating(0);
+    setSelectedSort("name");
+    setSelectedFaculty({ value: "none" });
+  };
 
   return (
     <div className={styles.container}>
@@ -103,12 +180,14 @@ const FilterModal = ({ onCancelButtonClick }: Props) => {
         <SelectBox
           label="Wydział"
           options={facultyOptions}
-          value={selectedFaculty}
-          onChange={(e) => setSelectedFaculty(e.target.value)}
+          value={selectedFaculty.value}
+          onChange={(e) => handleFacultyChange(e.target.value)}
         ></SelectBox>
       </div>
       <div className={styles.buttons}>
-        <Button style={ButtonStyle.BACKDROP}>Wyczyść</Button>
+        <Button style={ButtonStyle.BACKDROP} onClick={clearFilters}>
+          Wyczyść
+        </Button>
         <Button>Zastosuj</Button>
       </div>
     </div>
