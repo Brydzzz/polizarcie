5cbc5c500f3580a0593615aeaa6aaaeafae55ee8
Elbergg <tom.p.smolen@gmail.com>
2024-12-29 15:43:46 +0100

    match-list page

--- app/(content)/match-list/page.tsx
+++ app/(content)/match-list/page.tsx
@@ -1,9 +1,11 @@
 "use client";
+import ContactCard from "@/components/cards/contact-card.component.tsx";
 import MatchRequestCard from "@/components/cards/match-request.component";
 import { getPendingRequestsFor } from "@/lib/db/matches";
+import { getUsersMatchedWith } from "@/lib/db/users";
 import { useAppSelector } from "@/lib/store/hooks";
 import { selectCurrentUser } from "@/lib/store/user/user.selector";
-import { Match, User } from "@prisma/client";
+import { Match, User, UserMedia } from "@prisma/client";
 import { useEffect, useState } from "react";
 import styles from "./page.module.scss";
 const MatchRequestPage = () => {
@@ -11,25 +13,58 @@ const MatchRequestPage = () => {
   const [requests, setRequests] = useState<
     Array<Partial<Match & { userOne: Partial<User> }>>
   >([]);
+  const [contacts, setContacts] = useState<
+    Array<Partial<User & { medias: Partial<UserMedia>[] }>>
+  >([]);
   const user = useAppSelector(selectCurrentUser);
   useEffect(() => {
     const fetchReqs = async () => {
-      console.log(user);
       if (!user) return;
       const data = await getPendingRequestsFor(user?.id, 3);
-      console.log(data);
       setRequests(data);
     };
     fetchReqs();
   }, [user, decision]);
 
+  useEffect(() => {
+    const fetchMatches = async () => {
+      if (!user) return;
+      const data = await getUsersMatchedWith(user.id);
+      console.log(data);
+      setContacts(data);
+    };
+    fetchMatches();
+    console.log(contacts);
+  }, [user, decision]);
+
   return (
     <main className={styles.main}>
+      <div className={styles.prompt}>
+        <p>Zainteresowani wspólnym pożeraniem: </p>
+      </div>
       <div className={styles.requests} onClick={() => setDec(!decision)}>
         {requests.map((req, idx) =>
           req.userOne ? (
-            <MatchRequestCard key={idx} data={req} UserOne={req.userOne} />
+            <div className={styles.request} key={idx}>
+              <MatchRequestCard data={req} UserOne={req.userOne} />
+            </div>
           ) : null
+        )}
+      </div>
+      <div className={styles.prompt}>
+        <p>Kontakty: </p>
+      </div>
+      <div className={styles.contacts}>
+        {contacts.map((cont, idx) =>
+          cont.medias ? (
+            <div className={styles.contact} key={idx}>
+              <ContactCard data={cont} medias={cont.medias} />
+            </div>
+          ) : (
+            <div className={styles.contact} key={idx}>
+              <ContactCard data={cont} medias={[]} />
+            </div>
+          )
         )}
       </div>
     </main>
