0fcb5a8350e2251c96f857699b328224866c57a1
Elbergg <tom.p.smolen@gmail.com>
2024-12-30 16:45:25 +0100

    basic photos

--- components/cards/contact-card.component.tsx.tsx
+++ components/cards/contact-card.component.tsx.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { User, UserMedia } from "@prisma/client";
+import SupabaseImage from "../images/supabase-image.component";
 import styles from "./contact-card.module.scss";
 import LinkCard from "./link-card.component";
 type Props = {
@@ -12,7 +13,19 @@ const ContactCard = ({ data, medias }: Props) => {
     <div className={styles.container}>
       <div className={styles.left}>
         <div className={styles.photoBox}>
-          <div className={styles.photo}></div>
+          {data.image ? (
+            data.image.includes("https") ? (
+              <img src={data.image} width={30} height={30} alt="Zdjęcie" />
+            ) : (
+              <SupabaseImage
+                src={data.image}
+                width={30}
+                height={30}
+                quality={50}
+                alt="Zdjęcie"
+              />
+            )
+          ) : null}
         </div>
         <div className={styles.namebox}>
           {data ? <p className={styles.name}>{data.name}</p> : null}
--- components/cards/match-card-component.tsx
+++ components/cards/match-card-component.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Restaurant, User } from "@prisma/client";
+import SupabaseImage from "../images/supabase-image.component";
 import styles from "./match-card.module.scss";
 import UserCard from "./user-desc-card-component";
 type Props = {
@@ -12,7 +13,19 @@ const MatchCard = ({ data, likedRests }: Props) => {
   return (
     <div className={styles.container}>
       <div className={styles.photoBox}>
-        <div className={styles.photo}></div>
+        {data.image ? (
+          data.image.includes("https") ? (
+            <img src={data.image} width={162} height={162} alt="Zdjęcie" />
+          ) : (
+            <SupabaseImage
+              src={data.image}
+              width={162}
+              height={162}
+              quality={100}
+              alt="Zdjęcie"
+            />
+          )
+        ) : null}
       </div>
       <div className={styles.details}>
         <div className={styles.restaurants}>
