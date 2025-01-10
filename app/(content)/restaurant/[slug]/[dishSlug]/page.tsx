e3a1f5c1b053bed5598fc0dd3506c68263078ba4
Rys00 <mateusz.ogniewski@gmail.com>
2025-01-10 19:18:51 +0100

    finished dish card looks

--- app/(content)/restaurant/[slug]/[dishSlug]/page.tsx
+++ app/(content)/restaurant/[slug]/[dishSlug]/page.tsx
@@ -2,6 +2,7 @@ import StarInput from "@/components/inputs/star-input.component";
 import ReviewList from "@/components/lists/review-list.component";
 import { getDishBySlugs } from "@/lib/db/dishes";
 import { getRestaurantBySlug } from "@/lib/db/restaurants";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import styles from "./page.module.scss";
 
@@ -21,16 +22,23 @@ const DishPage = async ({ params }: Props) => {
 
   return (
     <div className={styles.container}>
-      <h1>{dish.name}</h1>
-      <span className={styles.rating}>
-        <StarInput
-          value={dish.averageStars || 0}
-          max={5}
-          starSize="24pt"
-          disabled
-        ></StarInput>
-        <p>{dish.averageStars || 0}</p>
-      </span>
+      <header className={styles.header}>
+        <Link href={`/restaurant/${restaurant.slug}`}>
+          <i className="fa-solid fa-arrow-left"></i>
+          <h2>{restaurant.name}</h2>
+        </Link>
+        <h1>{dish.name}</h1>
+        <p>Średnia ocena:</p>
+        <span className={styles.rating}>
+          <StarInput
+            value={dish.averageStars || 0}
+            max={5}
+            starSize="20pt"
+            disabled
+          />
+          <p>({dish.averageStars || 0})</p>
+        </span>
+      </header>
       <div className={styles.data}>
         <h2>Opis:</h2>
         <p>{dish.description}</p>
