c839e32b356e7579c44792be6da19ce68b5a9261
mdaszews <01180767@pw.edu.pl>
2025-01-10 23:23:27 +0100

    enhance sidebar with icons for better navigation

--- components/sidebars/dashboard-sidebar.component.tsx
+++ components/sidebars/dashboard-sidebar.component.tsx
@@ -6,10 +6,34 @@ const Sidebar = () => {
     return (
         <div className={styles.sidebar}>
             <h2>Panel ustawień</h2>
-            <span className={styles.item} onClick={() => (window.location.href = `#`)}>Podgląd</span>
-            <span className={styles.item} onClick={() => (window.location.href = `#`)}>Profil</span>
-            <span className={styles.item} onClick={() => (window.location.href = `#`)}>Ulubione restauracje</span>
-            <span className={styles.item} onClick={() => (window.location.href = `#`)}>Cos</span>
+            <span
+              className={styles.item}
+              onClick={() => (window.location.href = `#`)}
+            >
+              <i className="fa-solid fa-user"></i>
+              &nbsp; &nbsp; Podgląd
+            </span>
+            <span
+              className={styles.item}
+              onClick={() => (window.location.href = `#`)}
+            >
+              <i className="fa-solid fa-gear"></i>
+              &nbsp; &nbsp; Profil
+            </span>
+            <span
+              className={styles.item}
+              onClick={() => (window.location.href = `#`)}
+            >
+              <i className="fa-solid fa-heart"></i>
+              &nbsp; &nbsp; Ulubione
+            </span>
+            <span
+              className={styles.item}
+              onClick={() => (window.location.href = `#`)}
+            >
+              <i className="fa-solid fa-question"></i>
+              &nbsp; &nbsp; Cos
+            </span>
         </div>
     );
 };
