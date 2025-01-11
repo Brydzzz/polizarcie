c706e2db73663f9461b0f2fc8ec24f40d0f672b3
Rys00 <mateusz.ogniewski@gmail.com>
2025-01-11 12:35:36 +0100

    added git hub avatars to trusted image hostname

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,13 @@ const nextConfig: NextConfig = {
         pathname: "**",
         search: "",
       },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "**",
+        search: "",
+      },
     ],
   },
 };
